276b8e2c61cf260052e601508d6ce5d8b15cb485
gcsequino <gcsequino@gmail.com>
2022-03-05 16:33:29 -0700

    KB updates

--- app/src/main/java/scaling/utils/RandomBytes.java
+++ app/src/main/java/scaling/utils/RandomBytes.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomBytes {
     public static byte[] randBytes() {
         Random r = new Random();
-        byte[] bytes = new byte[8000];
+        byte[] bytes = new byte[8192];
         r.nextBytes(bytes);
         return bytes;
     }
