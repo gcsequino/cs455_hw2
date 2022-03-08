1bc765ca6c23aec9489398418b58563330c58d3a
gcsequino <gcsequino@gmail.com>
2022-03-08 14:19:21 -0700

    client update complete

--- app/src/main/java/scaling/client/StatsTask.java
+++ app/src/main/java/scaling/client/StatsTask.java
@@ -8,16 +8,23 @@ public class StatsTask extends TimerTask {
 
     private AtomicInteger sent_count;
     private AtomicInteger received_count;
+    private int reports;
 
     public StatsTask(AtomicInteger sent_count, AtomicInteger received_count) {
         this.sent_count = sent_count;
         this.received_count = received_count;
+        this.reports = 0;
     }
 
     private void printStats() {
+        reports++;
         Date curTime = new Date(System.currentTimeMillis());
         System.out.printf("[%s] Total Sent Count: %s, Total Received Count: %s\n", curTime, sent_count.toString(), received_count.toString());
-
+        if(reports >= 10) {
+            this.sent_count.set(0);
+            this.received_count.set(0);
+            reports = 0;
+        }
             //System.out.printf("[%d] HashQueue\n", curTime, );
     }
 
